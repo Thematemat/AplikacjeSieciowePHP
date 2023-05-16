10b0f2642f6b53d905a5532d7e49833a3dc4f1e1
Mateusz Turzyniecki <34538727+Thematemat@users.noreply.github.com>
2023-05-16 20:39:22 +0200

    Dodano widok pracownika

--- loginGate.php
+++ loginGate.php
@@ -15,7 +15,6 @@ $haslo = $_POST['haslo'];
 
 $db_lnk = mysqli_connect('localhost', 'root', '', 'imbiblio');
 $query = 'SELECT * FROM users';
-
 if (!$result = mysqli_query($db_lnk, $query))
 {
     echo 'Wystąpił błąd - nieprawidłowe zapytanie!';
@@ -23,12 +22,17 @@ if (!$result = mysqli_query($db_lnk, $query))
 else {
     $liczba_rekordow = mysqli_num_rows($result);
     $j = $liczba_rekordow - 1;
+
     for ($i = 0; $i < $liczba_rekordow; $i++) {
         $arr=mysqli_fetch_row($result);
+        $funkcja = $arr[3];
     if($login == $arr[1] && $haslo == $arr[2]){
         echo "<h3 style=text-align:right><a href =loginScreen.php>Wyloguj</a><br></h3>";
         echo "Zalogowano jako: $arr[1] <br>";
         echo "Bardzo dobra robota!";
+        if($funkcja == "pracownik"){
+            echo "<h3><a href =workerView.php>Przejdź do serwisu</a><br></h3>";
+        }
         break;
     }
     elseif($j == $i){
