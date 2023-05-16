9e2e40545afb6e6e9d6b98bbac95d3d3fb62dc43
Mateusz Turzyniecki <34538727+Thematemat@users.noreply.github.com>
2023-05-16 12:38:43 +0200

    database

---
+++ imbiblio.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 16 Maj 2023, 12:38
+-- Wersja serwera: 10.4.27-MariaDB
+-- Wersja PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `imbiblio`
+--
+CREATE DATABASE IF NOT EXISTS `imbiblio` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `imbiblio`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `czytelnicy`
+--
+
+CREATE TABLE `czytelnicy` (
+  `id` int(11) NOT NULL,
+  `imie` varchar(65) NOT NULL,
+  `nazwisko` varchar(65) NOT NULL,
+  `adres` varchar(65) NOT NULL,
+  `urodzony` date NOT NULL,
+  `nrDowodu` varchar(65) NOT NULL,
+  `nrTelefonu` int(11) NOT NULL,
+  `id_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(65) NOT NULL,
+  `password` varchar(65) NOT NULL,
+  `function` varchar(65) NOT NULL,
+  `id_reader` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `function`, `id_reader`) VALUES
+(1, 'admin', 'admin', 'pracownik', NULL),
+(2, 'user1', 'user1', 'czytelnik', NULL);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
